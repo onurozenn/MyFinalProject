fef6758cefbb0a3ceae9f70bcf46041be5bcc61c
Onur Özen <onur_ozen@outlook.com>
2021-03-14 22:41:06 +0300

    14 mart

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime ="Sistem bakımda";
         public static string ProductsListed ="Ürünler listelendi";
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir ";
+        public static string ProductNameAlreadyExists="bu isimde başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldı yeni ürün eklenemiyor";
     }
 }
